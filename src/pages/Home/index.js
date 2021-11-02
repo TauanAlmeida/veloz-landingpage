4f106f860d9c72ef30f2c17dfacefb4fe61b04e0
Tauan Almeida <tauan_almeida@hotmail.com>
2021-11-02 15:24:37 -0300

    finalizado landingpage

---
+++ src/pages/Home/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import logo from "../../assets/logo.svg";
+import mockup from "../../assets/mockup-celular.svg";
+
+function Home() {
+  return (
+    <div className="main">
+      <div className="container">
+        <div className="logo-container">
+          <img className="logo" src={logo} alt="Veloz" />
+          <p className="logo-texto">VELOZ</p>
+        </div>
+        <div className="main-content">
+          <div className="aside-left">
+            <img className="mackup-celular" src={mockup} alt="Mockup" />
+          </div>
+          <div className="aside-right">
+            <p className="font-title text-center">
+              Aumente sua perfomance com segurança, tecnologia e acompanhamento
+              de profissionais.
+            </p>
+            <a className="btn-download" href="app-universal-release.apk" target="_blank" download>baixar para android</a>
+            <button className="btn-disabled">baixar para ios (em breve)</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
